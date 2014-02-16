a92a99651dc03daf9c9db410c1f1efd312e56222
Sarah <zizi@cmu.edu>
2014-02-16 14:53:33 -0500

    image uploader with AWS storage

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
       :provider               => 'AWS',                             # required
-      :aws_access_key_id      => ENV['AW_ACCESS_KEY'],              # required
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],              # required
       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']       # required
   }
     config.fog_directory  = ENV['AWS_BUCKET']                       # required
